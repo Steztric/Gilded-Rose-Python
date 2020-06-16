1ba6944d33045a7cbe422411f32dc336f8e353ed
Patrick Morris <pm141088@gmail.com>
2020-06-16 11:46:48 +0100

    Abstracted decrement quality method. Function is called whenever we need to reduce quality
    Co-authored-by: Sterren Fisher <sterren.fisher@hotmail.co.uk>

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -3,13 +3,16 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
         self.maxQuality = 50
+    
+    def decrement_quality(self, item):
+        if item.name != "Sulfuras, Hand of Ragnaros" and item.quality > 0:
+            item.quality = item.quality - 1
+
 
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                self.decrement_quality(item)
             else:
                 if item.quality < self.maxQuality:
                     item.quality = item.quality + 1
@@ -25,9 +28,7 @@ class GildedRose(object):
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                        self.decrement_quality(item)
                     else:
                         item.quality = item.quality - item.quality
                 else:
